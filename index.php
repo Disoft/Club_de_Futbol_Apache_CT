99b85bcf18225e3855b9d141df04a984a697b479
Jorge Mostajo <Jorge Mostajo@JM-Escritorio>
2013-06-01 00:04:03 -0400

    comments

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once 'config/Config.php';
 require_once Config::get_value('adodb', 'adodb.inc.php');
 require_once Config::get_value('dbconnection', 'MySqlConnectionManager.php');
 require_once Config::get_value('smarty', 'Smarty.class.php');
-require_once Config::get_value('template', 'TemplateUtils.php');
+//require_once Config::get_value('template', 'TemplateUtils.php');
 //require_once 'classes/model/DataAccessModel.php'; /* General model class. */
 //require 'classes/controller/FrontController.php'; /* Application front controller. */
 
